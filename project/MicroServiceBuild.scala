98b90e66080e9b7630c5d1aa678036115b060dd0
Callum Turnbull <callum@turnbull.codes>
2016-10-12 11:26:34 +0100

    NISP-1949: Add auth filter

--- project/MicroServiceBuild.scala
+++ project/MicroServiceBuild.scala
@@ -21,7 +21,7 @@ private object AppDependencies {
   import play.core.PlayVersion
 
   private val microserviceBootstrapVersion = "4.2.1"
-  private val playAuthVersion = "3.1.0"
+  private val playAuthVersion = "3.4.0"
   private val playHealthVersion = "1.1.0"
   private val playJsonLoggerVersion = "2.1.1"  
   private val playUrlBindersVersion = "1.0.0"
