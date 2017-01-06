6a63166a1ef5b4e30080fc6e444b831b5d520592
Callum Turnbull <callum.turnbull@digital.hmrc.gov.uk>
2017-01-06 11:33:32 +0000

    Update Licence Headers

--- app/uk/gov/hmrc/statepension/controllers/StatePensionController.scala
+++ app/uk/gov/hmrc/statepension/controllers/StatePensionController.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 HM Revenue & Customs
+ * Copyright 2017 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -59,4 +59,3 @@ trait StatePensionController extends BaseController with HeaderValidator with Er
       })
   }
 }
-
--- app/uk/gov/hmrc/statepension/controllers/sandbox/StatePensionController.scala
+++ app/uk/gov/hmrc/statepension/controllers/sandbox/StatePensionController.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 HM Revenue & Customs
+ * Copyright 2017 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -37,4 +37,3 @@ object StatePensionController extends StatePensionController {
     override def sendEvent(event: AuditEvent)(implicit hc: HeaderCarrier): Unit = Logger.info(s"Sandbox Audit event sent: ${event.auditType}")
   }
 }
-
