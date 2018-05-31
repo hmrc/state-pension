811446f0dc1349ef2791d9c02c6d95a43257fa95
tonymyers <tony.myers@digital.hmrc.gov.uk>
2018-05-31 12:00:28 +0100

    DDCNLS-3418 Removed NPsConnector, NpsSummary Forecasting, ExclusionService and tests all relating to Nps. Also refactored ExclusionServiceSpec to test DesLiabilities.

--- app/uk/gov/hmrc/statepension/events/Forecasting.scala
+++ app/uk/gov/hmrc/statepension/events/Forecasting.scala
@@ -19,40 +19,8 @@ package uk.gov.hmrc.statepension.events
 import org.joda.time.LocalDate
 import uk.gov.hmrc.domain.Nino
 import uk.gov.hmrc.statepension.domain.Exclusion.Exclusion
-import uk.gov.hmrc.statepension.domain.nps.{DesAmountA2016, DesAmountB2016, NpsAmountA2016, NpsAmountB2016}
+import uk.gov.hmrc.statepension.domain.nps.{DesAmountA2016, DesAmountB2016}
 import uk.gov.hmrc.http.HeaderCarrier
-
-//todo:remove after test
-object Forecasting {
-  def apply(nino: Nino, earningsIncludedUpTo: LocalDate, currentQualifyingYears: Int, amountA: NpsAmountA2016, amountB: NpsAmountB2016,
-            finalRelevantYear: Int, exclusions: List[Exclusion])(implicit hc: HeaderCarrier): Forecasting =
-    new Forecasting(nino, earningsIncludedUpTo, currentQualifyingYears, amountA, amountB, finalRelevantYear, exclusions)
-}
-
-class Forecasting(nino: Nino, earningsIncludedUpTo: LocalDate, currentQualifyingYears: Int, amountA: NpsAmountA2016,
-                  amountB: NpsAmountB2016, finalRelevantYear: Int, exclusions: List[Exclusion])
-                 (implicit hc: HeaderCarrier)
-  extends BusinessEvent("Forecasting", nino, Map(
-    "nino" -> nino.value,
-    "earningsIncludedUpTo" -> earningsIncludedUpTo.toString,
-    "currentQualifyingYears" -> currentQualifyingYears.toString,
-    "amountAtotal" -> amountA.total.toString(),
-    "basicPension" -> amountA.basicStatePension.toString(),
-    "pre97AP" -> amountA.pre97AP.toString(),
-    "post97AP" -> amountA.post97AP.toString(),
-    "post02AP" -> amountA.post02AP.toString(),
-    "pre88GMP" -> amountA.pre88GMP.toString(),
-    "post88GMP" -> amountA.post88GMP.toString(),
-    "pre88COD" -> amountA.pre88COD.toString(),
-    "post88COD" -> amountA.post88COD.toString(),
-    "grb" -> amountA.graduatedRetirementBenefit.toString(),
-    "totalAP" -> amountA.totalAP.toString(),
-    "amountBmain" -> amountB.mainComponent.toString(),
-    "rda" -> amountB.rebateDerivedAmount.toString(),
-    "fry" -> finalRelevantYear.toString,
-    "exclusions" -> exclusions.map(_.toString).mkString(",")
-  ))
-
 
 object DesForecasting {
     def apply(nino: Nino, earningsIncludedUpTo: LocalDate, currentQualifyingYears: Int, amountA: DesAmountA2016, amountB: DesAmountB2016,
