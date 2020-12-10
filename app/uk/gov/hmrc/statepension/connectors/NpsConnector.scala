e16543c9ab3f46a08a54f053e4bf50f51e0cbf14
Kurt Logan <4375059+kurtlogan@users.noreply.github.com>
2020-12-10 08:53:42 +0000

    removed defaulting to Nil for 404

--- app/uk/gov/hmrc/statepension/connectors/NpsConnector.scala
+++ app/uk/gov/hmrc/statepension/connectors/NpsConnector.scala
@@ -20,7 +20,7 @@ import play.api.data.validation.ValidationError
 import play.api.libs.json.{JsPath, Reads}
 import uk.gov.hmrc.domain.Nino
 import uk.gov.hmrc.http.logging.Authorization
-import uk.gov.hmrc.http.{HeaderCarrier, HttpReads, HttpResponse, NotFoundException}
+import uk.gov.hmrc.http.{HeaderCarrier, HttpReads, HttpResponse}
 import uk.gov.hmrc.statepension.WSHttp
 import uk.gov.hmrc.statepension.domain.nps._
 import uk.gov.hmrc.statepension.services.ApplicationMetrics
@@ -48,9 +48,7 @@ trait NpsConnector {
     connectToHOD[Summary](summaryUrl(nino), summaryMetricType)
 
   def getLiabilities(nino: Nino)(implicit headerCarrier: HeaderCarrier): Future[List[Liability]] =
-    connectToHOD[Liabilities](liabilitiesUrl(nino), liabilitiesMetricType)
-      .map(_.liabilities)
-      .recover { case _: NotFoundException => Nil }
+    connectToHOD[Liabilities](liabilitiesUrl(nino), liabilitiesMetricType).map(_.liabilities)
 
   def getNIRecord(nino: Nino)(implicit headerCarrier: HeaderCarrier): Future[NIRecord] =
     connectToHOD[NIRecord](niRecordUrl(nino), niRecordMetricType)
