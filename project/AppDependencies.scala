c2549aff79a995c5aaa861748903a7732c6cbe50
matthew-mcnally <matthew.mcnally@digital.hmrc.gov.uk>
2022-11-02 11:22:09 +0000

    DDCNL-6458: Addressed PR bot comments

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -33,12 +33,9 @@ object AppDependencies {
   )
 
   val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc" %% "play-hal" % "3.1.0-play-28",
     "uk.gov.hmrc" %% "bootstrap-test-play-28" % "7.0.0",
     "org.mockito" % "mockito-core" % "4.7.0",
-    "com.github.tomakehurst" % "wiremock-jre8" % "2.26.3",
-    "org.pegdown" % "pegdown" % "1.6.0"
-
+    "com.github.tomakehurst" % "wiremock-jre8" % "2.26.3"
   ).map(_ % "test,it")
 
   private val silencerDependencies: Seq[ModuleID] = Seq(
