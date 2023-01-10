da9a2047a5083f30b4583b572b91d499ff7fa543
Ian Godman <IanPGodman@users.noreply.github.com>
2023-01-10 08:38:37 +0000

    Changes as suggest by platops-pr-bot

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -26,7 +26,6 @@ object AppDependencies {
     "uk.gov.hmrc" %% "play-hmrc-api" % "7.1.0-play-28",
     "uk.gov.hmrc" %% "play-hal" % "3.2.0-play-28",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28" % "0.71.0",
-//    "com.typesafe.play" %% "play-json" % "2.9.2",
     "commons-codec" % "commons-codec" % "1.15"
   )
 
