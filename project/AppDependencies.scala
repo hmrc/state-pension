ecf554b863131783c5278284a03c853a77e4ae28
Vincent Vinnicombe <5462800+vinnicombe@users.noreply.github.com>
2023-07-20 09:20:15 +0100

    DDCNL-7509: PR bot comments and updating deps

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -18,24 +18,24 @@ import play.sbt.PlayImport._
 import sbt._
 
 object AppDependencies {
+  val bootstrapVersion = "7.19.0"
 
   val compile: Seq[ModuleID] = Seq(
     ws,
-    "uk.gov.hmrc"       %%  "bootstrap-backend-play-28"     % "7.12.0",
-    "uk.gov.hmrc"       %%  "domain"                        % "8.1.0-play-28",
-    "uk.gov.hmrc"       %%  "play-hmrc-api"                 % "7.1.0-play-28",
-    "uk.gov.hmrc"       %%  "play-hal"                      % "3.2.0-play-28",
+    "uk.gov.hmrc"       %%  "bootstrap-backend-play-28"     % bootstrapVersion,
+    "uk.gov.hmrc"       %%  "domain"                        % "8.3.0-play-28",
+    "uk.gov.hmrc"       %%  "play-hmrc-api"                 % "7.2.0-play-28",
+    "uk.gov.hmrc"       %%  "play-hal"                      % "3.4.0-play-28",
     "uk.gov.hmrc"       %%  "mongo-feature-toggles-client"  % "0.2.0",
-    "uk.gov.hmrc.mongo" %%  "hmrc-mongo-play-28"            % "0.74.0",
     "commons-codec"     %   "commons-codec"                 % "1.15"
   )
 
   val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"             %%  "bootstrap-test-play-28"  % "7.12.0",
+    "uk.gov.hmrc"             %%  "bootstrap-test-play-28"  % bootstrapVersion,
     "org.mockito"             %   "mockito-core"            % "4.7.0",
     "com.github.tomakehurst"  %   "wiremock-jre8"           % "2.26.3",
-    "org.pegdown"             %   "pegdown"                 % "1.6.0"
-
+    "org.pegdown"             %   "pegdown"                 % "1.6.0",
+    "uk.gov.hmrc"             %%  "play-hal"                % "3.4.0-play-28"
   ).map(_ % "test,it")
 
   private val silencerDependencies: Seq[ModuleID] = Seq(
