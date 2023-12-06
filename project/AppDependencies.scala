7d350eeb212c7b286c8f07241ba300e58c99bc8a
Tateni93 <33066342+Tateni93@users.noreply.github.com>
2023-12-06 14:56:15 +0000

    DDCNL-8219: removed wiremock to remove jetty dependency vulnerability

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -18,7 +18,7 @@ import play.sbt.PlayImport._
 import sbt._
 
 object AppDependencies {
-  val bootstrapVersion = "7.21.0"
+  val bootstrapVersion = "7.23.0"
 
   val compile: Seq[ModuleID] = Seq(
     ws,
@@ -33,7 +33,6 @@ object AppDependencies {
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"             %%  "bootstrap-test-play-28"  % bootstrapVersion,
     "org.mockito"             %   "mockito-core"            % "4.7.0",
-    "com.github.tomakehurst"  %   "wiremock-jre8"           % "2.26.3",
     "org.pegdown"             %   "pegdown"                 % "1.6.0",
     "uk.gov.hmrc"             %%  "play-hal"                % "3.4.0-play-28"
   ).map(_ % "test,it")
