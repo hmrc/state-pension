848660b15e51ef59859537fdcafe19583221ca9f
Vincent Vinnicombe <5462800+vinnicombe@users.noreply.github.com>
2024-04-12 14:31:25 +0100

    DDCNL-8352: Fixed ITs

--- it/test/config/InternalAuthInitializerISpec.scala
+++ it/test/config/InternalAuthInitializerISpec.scala
@@ -1,116 +1,112 @@
-package test.config
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
+package config
+
+import com.github.tomakehurst.wiremock.client.WireMock._
 import org.apache.pekko.Done
-import com.github.tomakehurst.wiremock.client.WireMock.{aResponse, get, getRequestedFor, post, postRequestedFor, urlMatching}
 import org.scalatest.concurrent.ScalaFutures.convertScalaFuture
-import org.scalatestplus.play.guice.GuiceOneAppPerSuite
-import play.api.cache.AsyncCacheApi
 import play.api.http.Status.{CREATED, NOT_FOUND, OK}
-import play.api.inject.bind
 import play.api.inject.guice.GuiceApplicationBuilder
+import play.api.test.Helpers.running
+import uk.gov.hmrc.http.test.WireMockSupport
 import uk.gov.hmrc.statepension.config.InternalAuthTokenInitializer
-import utils.{ResponseHelpers, StatePensionBaseSpec, WireMockHelper}
+import utils.StatePensionBaseSpec
 
-class InternalAuthInitializerISpec extends StatePensionBaseSpec
-  with GuiceOneAppPerSuite
-  with WireMockHelper
-  with ResponseHelpers {
+class InternalAuthInitializerISpec
+  extends StatePensionBaseSpec
+    with WireMockSupport {
+
+  private def builder(isTestOnlyEndpoint: Boolean): GuiceApplicationBuilder =
+    GuiceApplicationBuilder()
+      .configure(
+        "internal-auth.isTestOnlyEndpoint"         -> isTestOnlyEndpoint,
+        "microservice.services.internal-auth.port" -> wireMockPort
+      )
+
   "AuthTokenInitializer" should {
     "return Done with no requests sent to internal-auth" when {
       "isTestEndpoints is configured to false" in {
 
-        val authToken = "authToken"
-        val appName = "appName"
-
-        server.stubFor(
+        wireMockServer.stubFor(
           get(urlMatching("/test-only/token"))
             .willReturn(aResponse().withStatus(OK))
         )
 
-        server.stubFor(
+        wireMockServer.stubFor(
           post(urlMatching("/test-only/token"))
             .willReturn(aResponse().withStatus(CREATED))
         )
 
-        val app = GuiceApplicationBuilder()
-          .overrides(
-            bind[AsyncCacheApi].toInstance(mockCacheApi)
-          )
-          .configure(
-            "microservice.services.internal-auth.port" -> server.port(),
-            "appName" -> appName,
-            "internal-auth.token" -> authToken,
-          )
-          .build()
+        val app = builder(isTestOnlyEndpoint = false).build()
 
-        app.injector.instanceOf[InternalAuthTokenInitializer].initializeToken.futureValue shouldBe Done
+        running(app) {
+          app.injector.instanceOf[InternalAuthTokenInitializer].initializeToken.futureValue shouldBe Done
 
-        server.verify(0, getRequestedFor(urlMatching("/test-only/token")))
-        server.verify(0, postRequestedFor(urlMatching("/test-only/token")))
+          wireMockServer.verify(0, getRequestedFor(urlMatching("/test-only/token")))
+          wireMockServer.verify(0, postRequestedFor(urlMatching("/test-only/token")))
+        }
       }
+    }
 
+    "return Done with one request sent to internal-auth" when {
       "isTestEndpoints is configured to true and token is already valid" in {
-        val authToken = "authToken"
-        val appName = "appName"
 
-        server.stubFor(
+        wireMockServer.stubFor(
           get(urlMatching("/test-only/token"))
             .willReturn(aResponse().withStatus(OK))
         )
 
-        server.stubFor(
+        wireMockServer.stubFor(
           post(urlMatching("/test-only/token"))
             .willReturn(aResponse().withStatus(CREATED))
         )
 
-        val app = GuiceApplicationBuilder()
-          .overrides(
-            bind[AsyncCacheApi].toInstance(mockCacheApi)
-          )
-          .configure(
-            "microservice.services.internal-auth.port" -> server.port(),
-            "appName" -> appName,
-            "internal-auth.token" -> authToken,
-            "internal-auth.isTestOnlyEndpoint" -> true
-          )
-          .build()
+        val app = builder(isTestOnlyEndpoint = true).build()
 
-        app.injector.instanceOf[InternalAuthTokenInitializer].initializeToken.futureValue shouldBe Done
+        running(app) {
+          app.injector.instanceOf[InternalAuthTokenInitializer].initializeToken.futureValue shouldBe Done
 
-        server.verify(1, getRequestedFor(urlMatching("/test-only/token")))
-        server.verify(0, postRequestedFor(urlMatching("/test-only/token")))
+          wireMockServer.verify(1, getRequestedFor(urlMatching("/test-only/token")))
+          wireMockServer.verify(0, postRequestedFor(urlMatching("/test-only/token")))
+        }
       }
+    }
 
+    "return Done with two requests sent to internal-auth" when {
       "isTestEndpoints is configured to true and token needs intitializing" in {
-        val authToken = "authToken"
-        val appName = "appName"
 
-        server.stubFor(
+        wireMockServer.stubFor(
           get(urlMatching("/test-only/token"))
             .willReturn(aResponse().withStatus(NOT_FOUND))
         )
 
-        server.stubFor(
+        wireMockServer.stubFor(
           post(urlMatching("/test-only/token"))
             .willReturn(aResponse().withStatus(CREATED))
         )
 
-        val app = GuiceApplicationBuilder()
-          .overrides(
-            bind[AsyncCacheApi].toInstance(mockCacheApi)
-          )
-          .configure(
-            "microservice.services.internal-auth.port" -> server.port(),
-            "appName" -> appName,
-            "internal-auth.token" -> authToken,
-            "internal-auth.isTestOnlyEndpoint" -> true
-          )
-          .build()
+        val app = builder(isTestOnlyEndpoint = true).build()
 
-        app.injector.instanceOf[InternalAuthTokenInitializer].initializeToken.futureValue shouldBe Done
+        running(app) {
+          app.injector.instanceOf[InternalAuthTokenInitializer].initializeToken.futureValue shouldBe Done
 
-        server.verify(1, getRequestedFor(urlMatching("/test-only/token")))
-        server.verify(1, postRequestedFor(urlMatching("/test-only/token")))
+          wireMockServer.verify(1, getRequestedFor(urlMatching("/test-only/token")))
+          wireMockServer.verify(1, postRequestedFor(urlMatching("/test-only/token")))
+        }
       }
     }
   }
