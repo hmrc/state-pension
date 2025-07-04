0784b7afc79db15e584b2133add72e65e3beba95
AndrewBrown22 <45789050+andrewbrown22@users.noreply.github.com>
2025-07-04 14:20:54 +0100

    DDCNL-10630B sort sbt file and upgrade version

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -20,7 +20,7 @@ import sbt.*
 object AppDependencies {
   val bootstrapVersion = "9.13.0"
   val playVersion = "play-30"
-  val hmrcMongoVersion = "2.5.0"
+  val hmrcMongoVersion = "2.6.0"
 
   val compile: Seq[ModuleID] = Seq(
     caffeine,
